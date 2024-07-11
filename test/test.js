bd73ddece4cdc4738aa7e6434b8e38b37d6b002f
Michaël Zasso <targos@protonmail.com>
2024-07-11 09:51:32 +0200

    feat!: update ESLint to flat config

---
+++ test/test.js
@@ -0,0 +1,21 @@
+import assert from 'node:assert';
+
+import { loadESLint } from 'eslint';
+
+const ESLint = await loadESLint({ useFlatConfig: true });
+const eslint = new ESLint();
+const formatter = await eslint.loadFormatter('stylish');
+
+const result = await eslint.lintFiles(['test/**/*.{gql,graphql}']);
+
+assert.strictEqual(result.length, 3, '3 graphql files should be linted');
+
+const errorCount = result.reduce(
+  (prev, current) => prev + current.errorCount,
+  0,
+);
+assert.strictEqual(
+  errorCount,
+  0,
+  `graphql files should not have any errors: ${formatter.format(result)}`,
+);
